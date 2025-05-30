7e14d011a3dbe860f07729ee4633b54848b517d4
Albin Aji <albinpariyarathu@gmail.com>
2025-05-30 22:54:25 +0530

    modified:   switch-go/main.go

--- switch-go/main.go
+++ switch-go/main.go
@@ -1,12 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	// normal switch
 	i := 1
 	switch i {
 	case 0:
 		fmt.Println("Number is 0")
 		break
+	}
+
+	fmt.Println(time.Now().Weekday())
+
+	// multiple case matching
+	switch time.Now().Weekday() {
+	case time.Thursday, time.Friday:
+		fmt.Println("This is a friday")
+	case time.Saturday, time.Sunday:
+		fmt.Println("This is a weekend")
+	default:
+		fmt.Println("This is a weekday")
+	}
+
+	// switch like if-else
+	t := time.Now()
+	fmt.Println(t.Hour())
+	switch {
+	case t.Hour() > 12:
+		fmt.Println("This is after noon")
+	default:
+		fmt.Println("This is before noon")
 	}
 }
