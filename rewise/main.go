f5a8715fb5099572038939ad53a894b0f1a78f68
Albin Aji <albinpariyarathu@gmail.com>
2026-02-17 18:13:04 +0530

    rewising go funcdamentals

--- rewise/main.go
+++ rewise/main.go
@@ -3,14 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	// fmt.Println("Hello")
+	var name string = "Albin"
 
-	// var age int
+	fmt.Println(name)
 
-	// fmt.Println(age)
+	print_name(name)
 
-	age := 10
+	sum := find_sum(10, 20)
 
-	fmt.Println(age)
+	fmt.Println("Sum is =", sum)
 
+
+	grouped_declaration()
+}
+
+func print_name(name string) {
+	fmt.Println("Hello ", name)
+}
+
+func find_sum(num1 int, num2 int) int {
+	return  num1 + num2
+}
+
+func grouped_declaration() {
+	var x, y, z = 1, 2, 3
+
+	var (
+		a int
+		b string
+		c float32 = 5
+	)
+
+	fmt.Println("grouped declaration =>", a, b, c)
+
+
+	fmt.Println(x, y, z)
 }
